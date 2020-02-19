8c5c94a0efc367a05d8b4ae8de60e97c31a1aa80
Steve Vance <steve.vance@hotmail.co.uk>
2020-02-19 17:22:37 +0000

    homework questions added

---
+++ homework_polymorphism_composition_hw.js
@@ -0,0 +1,43 @@
+# Polymorphism & Composition Homework - Quiz
+
+# Polymorphism
+
+1. What does the ___word___ 'polymorphism' mean?
+
+ - Polymorphism literally means something can have many forms.
+
+2. What does it mean when we apply polymorphism to OO design? Give a simple Java example.
+
+ - In 'OO' design the application of polymorphism allows us to treat a class as a different class or type and means we can stick to SOLID principles making our code more extensible.
+
+3. What can we use to implement polymorphism in Java?
+
+ - We can implement polymorphism in Java through the use of Abstract classes and Interfaces.
+
+4. How many 'forms' can an object take when using polymorphism?
+
+ - We can treat an instance of a class as if it is another two forms; a new class or a new type at the same time.
+
+5. Give an example of when you could use polymorphism.
+
+ - We could create an abstract class. A superclass which all the classes we want to treat as being the same can inherit methods from.
+
+
+
+# Composition
+
+6. What do we mean by 'composition' in reference to object-oriented programming?
+
+ - Composition refers to a HAS-A relationship where an object is made up of one or more other objects.
+
+7. When would you use composition? Provide a simple example in Java.
+
+ - We can use composition in cases where one object "has" (or is part of) another object.
+
+8. What is/are the advantage(s) of using composition?
+
+- Composition allows a class to use behaviour from a group of other classes, and makes it possible for that behaviour to change at runtime.
+
+9. When an object is destroyed, what happens to all the objects it is composed of?
+
+ - If the composite object is destroyed, all the component parts must be destroyed. The part has no life of itself and cannot be transferred to another object.
